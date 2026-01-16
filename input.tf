42bd6097c74ae8b20486f610a085969741a556f4
Rajesh Jammalamadaka <rajesh.jammalamadaka@keyloop.com>
2026-01-16 10:51:10 +0530

    added module folder

---
+++ input.tf
@@ -0,0 +1,11 @@
+variable "cidr_value" {
+  description = "cidr range for vpc"
+}
+
+variable "ami_value" {
+  description = "ami_value for ec2"
+}
+
+variable "instance_type_value" {
+  description = "instance type for ec2"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-0ecb62995f68bb549"
+instance_type_value = "t2.micro"
+cidr_value = "10.0.0.0/16"
