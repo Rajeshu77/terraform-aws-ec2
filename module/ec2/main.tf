1bec0f28f523fe54142262d3ac3a6ce3faf09359
Rajesh Jammalamadaka <rajesh.jammalamadaka@keyloop.com>
2026-01-20 11:34:42 +0530

    added files module

---
+++ module/ec2/main.tf
@@ -0,0 +1,77 @@
+# ec2 code
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = var.cidr_value
+}
+
+resource "aws_subnet" "sb1" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = aws_vpc.my-vpc.id
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "ig1" {
+  vpc_id = aws_vpc.my-vpc.id
+}
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig1.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id = aws_subnet.sb1.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_security_group" "sg1-my" {
+  name = "my-secrity-grup"
+  vpc_id = aws_vpc.my-vpc.id
+
+  ingress {
+    description = "allow 22"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow 80"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "allow-from out"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2-my" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  subnet_id = aws_subnet.sb1.id
+  vpc_security_group_ids = [aws_security_group.sg1-my.id]
+
+  tags = {
+    name = "my-terraform-ec2"
+  }
+}
+
+
+
+
---
+++ module/ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "cidr_value" {
+  description = "value for cidr vpc"
+}
+
+variable "ami_value" {
+  description = "value for ami"
+}
+
+variable "instance_type_value" {
+  description = "value for instance type"
+}
